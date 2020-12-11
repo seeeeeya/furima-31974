931b1b498ee61a1e8cb68a94d982bba18dba11b0
seiya <74032023+seeeeeya@users.noreply.github.com>
2020-12-11 18:29:08 +0900

    ユーザー管理機能修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :last_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
+    
+  with_options presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/,message: "全角(ひらがな カタカナ 漢字)が使えます"} do
+    validates :family_name
+    validates :last_name
+  end
 
-  has_many :items
-  has_many :purchases
+  with_options presence: true,format: { with: /\A[ァ-ヶー－]+\z/,message: "全角カタカナのみが使えます"} do
+    validates :family_name_kana
+    validates :last_name_kana
+  end
 
+    has_many :items
+    has_many :purchases
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,41 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include "Email can't be blank"
     end
+    it "email一意性であること" do
+      @user.email = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it "emailには@を含む" do
+      @user.email = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it "passwordが6文字以上でないと登録できない" do
+      @user.password = "aaaaa"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+    end
+    it "passwordが半角英語のみでは登録できない" do
+      @user.password = "aaaaa"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+    end
+    it "passwordが半角数字のみでは登録できない" do
+      @user.password = "11111"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+    end
+    it "passwordが全角では登録できない" do
+      @user.password = "かな"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+    end
+    it "passwordとencrypted_passwordは値が同じでないと登録できない" do
+      @user.password = 
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password can't be blank"
+    end
     it "encrypted_passwordが空では登録できない" do
       @user.encrypted_password = ""
       @user.valid?
@@ -30,6 +65,11 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include "Password can't be blank"
     end
+    it "family_nameとlast_nameは漢字、平仮名、カタカナ以外では登録できない" do
+      @user.family_name = "abc"
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password can't be blank"
+    end
     it "family_name_kanaが空では登録できない" do
       @user.family_name_kana = ""
       @user.valid?
@@ -37,6 +77,11 @@ RSpec.describe User, type: :model do
     end
     it "last_name_kanaが空では登録できない" do
       @user.last_name_kana = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password can't be blank"
+    end
+    it "family_name_kanaとlast_name_kanaは全角カタカナ以外では登録できない" do
+      @user.family_name_kana = "あ"
       @user.valid?
       expect(@user.errors.full_messages).to include "Password can't be blank"
     end
