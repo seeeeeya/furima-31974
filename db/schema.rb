1e58063d5dc1acab52c45a5bcd85ebab7443693b
seiya <74032023+seeeeeya@users.noreply.github.com>
2020-12-15 12:29:00 +0900

    モデルとテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_024507) do
+ActiveRecord::Schema.define(version: 2020_12_15_031130) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description_of_item", null: false
+    t.integer "category_id", null: false
+    t.integer "product_condition_id", null: false
+    t.integer "shipping_charges_id", null: false
+    t.integer "prefectures_id", null: false
+    t.integer "days_to_ship_id", null: false
+    t.integer "price", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
